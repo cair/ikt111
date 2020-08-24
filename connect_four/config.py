918b8d98d2cb50c9af5bddc16663710236059a6a
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-08-24 21:37:03 +0200

    Add config file

--- connect_four/config.py
+++ connect_four/config.py
@@ -1,4 +1,5 @@
 WIDTH  = 589
+<<<<<<< HEAD
 HEIGHT = 540
 PIECE_SIZE = 63 # Hardcoded to fit with background
 
@@ -7,5 +8,10 @@ COLS = 7
 
 D_X = 81   # How much you have to move right each col to fit
 D_y = 80.8 # How much you have to move down each row to fit
+=======
+HEIGHT = 529
+
+PIECE_SIZE = 63
+>>>>>>> b16e2cf... Add config file
 
 CLOCK_SPEED = 10
