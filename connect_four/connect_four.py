6023175800086e38918cacb11b2f06a5d5c2813d
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-08-25 22:28:18 +0200

    Some more boilerplate

--- connect_four/connect_four.py
+++ connect_four/connect_four.py
@@ -1,11 +1,16 @@
+<<<<<<< HEAD
 import os
 import numpy as np
 import pygame
 pygame.init()
+=======
+import pygame
+>>>>>>> 124ade9... Some more boilerplate
 
 import config
 
 colors = {
+<<<<<<< HEAD
     'black': (0, 0, 0),
     'white': (255, 255, 255),
     'blue':  (0, 0, 255)
@@ -40,15 +45,31 @@ class YellowPiece(pygame.sprite.Sprite):
         self.image = pygame.image.load(os.path.join(gfx_dir, 'yellow_piece.png'))
         self.rect = self.image.get_rect()
         self.rect.center = location
+=======
+    'white': (255, 255, 255),
+    'blue': (0, 0, 255)
+}
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location=[0, 0]):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+>>>>>>> 124ade9... Some more boilerplate
 
 
 class ConnectFour():
     def __init__(self):
+<<<<<<< HEAD
         self.font_style = pygame.font.SysFont(None, 80)
+=======
+>>>>>>> 124ade9... Some more boilerplate
         self.width   = config.WIDTH
         self.height  = config.HEIGHT
         self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Connect Four')
+<<<<<<< HEAD
         self.background = Background()
         self.clock = pygame.time.Clock()
         self.piece_size = config.PIECE_SIZE
@@ -59,6 +80,18 @@ class ConnectFour():
 
 
     def _display_message(self, msg, color=colors['black']):
+=======
+        self.background = Background('gfx/background.png')
+
+        self.clock = pygame.time.Clock()
+
+        self.piece_size = config.PIECE_SIZE
+
+        self.ai = lambda placeholder: self._game_over(msg='No AI registered!')
+
+
+    def _display_message(self, msg, color=colors['blue']):
+>>>>>>> 124ade9... Some more boilerplate
         """Helper function to show message on display"""
         message = self.font_style.render(msg, True, color)
         message_rect = message.get_rect(center=(self.width / 2, self.height / 2))
@@ -73,9 +106,12 @@ class ConnectFour():
         self.display.fill(colors['white'])
         self.display.blit(self.background.image, 
                             self.background.rect)
+<<<<<<< HEAD
 
         for piece in self.game_pieces:
             self.display.blit(piece.image, piece.rect)
+=======
+>>>>>>> 124ade9... Some more boilerplate
         pygame.display.update()
 
 
