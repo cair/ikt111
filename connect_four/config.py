d6830b871066bdab290791c038174465a921f805
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-08-25 22:28:44 +0200

    Update background

--- connect_four/config.py
+++ connect_four/config.py
@@ -1,5 +1,6 @@
 WIDTH  = 589
 <<<<<<< HEAD
+<<<<<<< HEAD
 HEIGHT = 540
 PIECE_SIZE = 63 # Hardcoded to fit with background
 
@@ -10,6 +11,9 @@ D_X = 81   # How much you have to move right each col to fit
 D_y = 80.8 # How much you have to move down each row to fit
 =======
 HEIGHT = 529
+=======
+HEIGHT = 540
+>>>>>>> 21d3231... Update background
 
 PIECE_SIZE = 63
 >>>>>>> b16e2cf... Add config file
