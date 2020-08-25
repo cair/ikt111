4294ef35010609044cd55bdd1113d65c9e3aba11
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-08-25 22:19:50 +0200

    Add sprites for pieces

--- connect_four/connect_four.py
+++ connect_four/connect_four.py
@@ -1,36 +1,64 @@
+import os
+import numpy as np
 import pygame
+pygame.init()
 
 import config
 
 colors = {
+    'black': (0, 0, 0),
     'white': (255, 255, 255),
-    'blue': (0, 0, 255)
+    'blue':  (0, 0, 255)
 }
 
+pieces = {
+    'board': 0,
+    'player1': 1,
+    'player2': 2
+}
+
+gfx_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gfx')
+
+
 class Background(pygame.sprite.Sprite):
-    def __init__(self, image_file, location=[0, 0]):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
+        self.image = pygame.image.load(os.path.join(gfx_dir, 'background.png'))
         self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+        self.rect.left, self.rect.top = [0, 0]
+
+class RedPiece(pygame.sprite.Sprite):
+    def __init__(self, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(gfx_dir, 'red_piece.png'))
+        self.rect = self.image.get_rect()
+        self.rect.center = location
+
+class YellowPiece(pygame.sprite.Sprite):
+    def __init__(self, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(gfx_dir, 'yellow_piece.png'))
+        self.rect = self.image.get_rect()
+        self.rect.center = location
 
 
 class ConnectFour():
     def __init__(self):
+        self.font_style = pygame.font.SysFont(None, 80)
         self.width   = config.WIDTH
         self.height  = config.HEIGHT
         self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Connect Four')
-        self.background = Background('gfx/background.png')
-
+        self.background = Background()
         self.clock = pygame.time.Clock()
-
         self.piece_size = config.PIECE_SIZE
 
+        self.game_pieces = []
+        self.board = [[pieces['board'] for _ in range(config.ROWS)] for _ in range(config.COLS)]
         self.ai = lambda placeholder: self._game_over(msg='No AI registered!')
 
 
-    def _display_message(self, msg, color=colors['blue']):
+    def _display_message(self, msg, color=colors['black']):
         """Helper function to show message on display"""
         message = self.font_style.render(msg, True, color)
         message_rect = message.get_rect(center=(self.width / 2, self.height / 2))
@@ -45,6 +73,9 @@ class ConnectFour():
         self.display.fill(colors['white'])
         self.display.blit(self.background.image, 
                             self.background.rect)
+
+        for piece in self.game_pieces:
+            self.display.blit(piece.image, piece.rect)
         pygame.display.update()
 
 
