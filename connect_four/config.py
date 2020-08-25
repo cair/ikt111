2f52b8896d022362d066f42e90cb5d77b1b71c7f
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-08-25 22:33:27 +0200

    More strange stuff happened... Git is hard...

--- connect_four/config.py
+++ connect_four/config.py
@@ -1,6 +1,4 @@
 WIDTH  = 589
-<<<<<<< HEAD
-<<<<<<< HEAD
 HEIGHT = 540
 PIECE_SIZE = 63 # Hardcoded to fit with background
 
@@ -9,13 +7,5 @@ COLS = 7
 
 D_X = 81   # How much you have to move right each col to fit
 D_y = 80.8 # How much you have to move down each row to fit
-=======
-HEIGHT = 529
-=======
-HEIGHT = 540
->>>>>>> 21d3231... Update background
-
-PIECE_SIZE = 63
->>>>>>> b16e2cf... Add config file
 
 CLOCK_SPEED = 10
