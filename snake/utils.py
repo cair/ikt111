76de6254f21b96778292246051e89434298593c6
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-09-16 21:50:32 +0200

    pos_to_int and rand_p now require that we pass sprite_size as parameter

--- snake/utils.py
+++ snake/utils.py
@@ -3,12 +3,13 @@ import random
 import config
 
 
-def pos_to_int(pos):
+def pos_to_int(pos, sprite_size):
     """Helper function that converts position values to integer"""
-    return (int(pos[0] / config.SNAKE_SIZE), 
-            int(pos[1] / config.SNAKE_SIZE))
+    return (int(pos[0] / sprite_size), 
+            int(pos[1] / sprite_size))
 
 
-def rand_p(_max):
+def rand_p(_max, sprite_size):
     """Helper function to generate a random position value""" 
-    return int(round(random.randrange(0, _max - config.SNAKE_SIZE) / config.SNAKE_SIZE) * config.SNAKE_SIZE)
+    return int(round(random.randrange(0, _max - sprite_size) / sprite_size) * sprite_size)
+
