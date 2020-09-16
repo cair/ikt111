b94f28b44b763b10dfcbf493b5ded301e1abd078
Jahn Thomas Fidje <jtfidje@gmail.com>
2020-09-16 21:52:01 +0200

    Add function to calculate GCD with two requirements:
    - GCD as close to, but not greater than, a given target
    - GCD greater than a given minimum

--- snake/utils.py
+++ snake/utils.py
@@ -13,3 +13,18 @@ def rand_p(_max, sprite_size):
     """Helper function to generate a random position value""" 
     return int(round(random.randrange(0, _max - sprite_size) / sprite_size) * sprite_size)
 
+
+def find_common_divisor(a, b, target, _min):
+    """Finds the common divisor that is closest to the given target"""
+    if target >= min(a, b):
+        return None
+
+    greatest = 1
+    for i in range(2, min(a, b) + 1): 
+        if i > target:
+            break
+        if (a % i == b % i == 0): 
+            greatest = i
+    if greatest < _min:
+        return None
+    return greatest
