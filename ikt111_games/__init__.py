a7f7adc65d2dc090e3d5d0d10c445226e5d1ca2e
Jahn Thomas Fidje <jtfidje@gmail.com>
2021-09-07 22:02:23 +0200

    Update main __init__

--- ikt111_games/__init__.py
+++ ikt111_games/__init__.py
@@ -1,3 +1,3 @@
-from .connect_four.connect_four import ConnectFour
-from .flappy.flappy import Flappy
-from .snake.snake import SnakeGame as Snake
+from ikt111_games.connect_four.connect_four import ConnectFour
+from ikt111_games.flappy.flappy import Flappy
+from ikt111_games.snake.snake import SnakeGame as Snake
