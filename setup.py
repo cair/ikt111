5d6349a5a646b9d9de2aa6d476b5c93b694b3e42
Jahn Thomas Fidje <jtfidje@gmail.com>
2021-09-07 21:44:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="ikt111_games",
-    version="1.0.0"
+    name="ikt111",
+    version="1.0.0",
+    description="Games for IKT111",
+    author="Jahn Thomas Fidje, Nicolai Eliassen Pedersen",
+    author_email="jtfidje@gmail.com, nicolaiep@uia.no",
+    packages=["ikt111_games"],
+    install_requires=[
+        "numpy==1.19.1",
+        "pygame==2.0.0"
+    ]
 )
